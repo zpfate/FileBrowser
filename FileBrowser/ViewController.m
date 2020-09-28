1f45ed524378eef749deaa32871c18bd07a54ed3
ZpFate <zptwistedfate@163.com>
2020-09-28 08:23:11 +0800

    加载本地pdf

--- FileBrowser/ViewController.m
+++ FileBrowser/ViewController.m
@@ -22,12 +22,15 @@
     // Do any additional setup after loading the view.
     
     [self setupViews];
-    [self loadLocalPDFFile];
+    [self loadPDFData];
 }
 
 - (void)setupViews {
     [self.view addSubview:self.wkWebView];
 }
+
+
+#pragma mark -- 加载本地PDF文件
 
 - (void)loadLocalPDFFile {
     
@@ -35,6 +38,16 @@
     NSURL *fileUrl = [NSURL fileURLWithPath:pdfPath];
     [self.wkWebView loadFileURL:fileUrl allowingReadAccessToURL:fileUrl];
 }
+
+#pragma mark -- PDF乱码加载二进制文件
+
+- (void)loadPDFData {
+    NSString *pdfPath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"pdf"];
+    NSData *pdfData = [[NSData alloc] initWithContentsOfFile:pdfPath];
+    [self.wkWebView loadData:pdfData MIMEType:@"application/pdf" characterEncodingName:@"GBK" baseURL:[NSURL URLWithString:pdfPath]];
+}
+
+#pragma mark -- 加载网络PDF文件
 
 - (void)downloadFile {
     
@@ -62,5 +75,17 @@
 }
 
 
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSString *_webUrlStr = navigationAction.request.URL.absoluteString;
+    NSString *lastName =[[_webUrlStr lastPathComponent] lowercaseString];
+    
+//    if ([lastName containsString:@".pdf"])
+//    {
+//        NSData *data = [NSData dataWithContentsOfURL:navigationAction.request.URL];
+//        [self.wkWebView loadData:data MIMEType:@"application/pdf" characterEncodingName:@"GBK" baseURL:[NSURL URLWithString:_webUrlStr]];
+//    }
+    decisionHandler(WKNavigationActionPolicyAllow);
 
+}
 @end
